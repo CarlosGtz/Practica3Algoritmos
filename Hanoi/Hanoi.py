8f1fec2f0ddfe8d06c1eb592185b5b4f26b5afb8
Carlos Gtz <carlos062695@gmail.com>
2016-10-18 10:54:23 -0500

    Primer commit

---
+++ Hanoi/Hanoi.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+from time import time
+
+TAM = int(input("FNumero de discos>>"))
+	
+def Hanoi(n, origen, auxiliar, destino):
+	if n ==1 :
+		print ("mover disco de " + str(origen) + " a " + str(destino))
+	else:
+		Hanoi(n-1, origen, destino, auxiliar)
+		print ("mover disco de " + str(origen) + " a " + str(destino))
+		Hanoi(n-1, auxiliar, origen, destino)
+
+def writeFile(fTime):
+	archivo = open("Hanoi.txt", "r+")
+	contenido = archivo.read()
+	final_de_archivo = archivo.tell()	 
+	archivo.write(fTime+" "+str(TAM)+"\n")
+	archivo.seek(final_de_archivo)
+	archivo.close()
+
+init_time = time()
+
+Hanoi(TAM,1,2,3)
+final_time = time()
+e_time = final_time - init_time
+str_time = str("%.20f" % e_time)
+writeFile(str_time)
+print "Time: %.20f" % e_time
+
+    
+    
+  
